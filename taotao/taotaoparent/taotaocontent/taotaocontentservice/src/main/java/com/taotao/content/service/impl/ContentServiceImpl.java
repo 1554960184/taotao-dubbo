5d15f3a31b1e982a1d0e5441de8b12957e10134a
scxiong <1554960184@qq.com>
2020-09-19 17:14:59 +0800

    大广告位使用redis做缓存

--- taotao/taotaoparent/taotaocontent/taotaocontentservice/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao/taotaoparent/taotaocontent/taotaocontentservice/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -3,11 +3,16 @@ package com.taotao.content.service.impl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.content.service.ContentService;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.EasyUIDataGridResult;
 import com.taotao.pojo.TaotaoResult;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
+import com.taotao.utils.JsonUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +24,10 @@ import java.util.List;
 public class ContentServiceImpl implements ContentService {
     @Resource
     private TbContentMapper contentMapper;
+    @Autowired
+    private JedisClient jedisClient;
+    @Value("${INDEX_CONTENT}")
+    private String INDEX_CONTENT;
     @Override
     public TaotaoResult addContent(TbContent content) {
         //补全pojo的属性
@@ -26,6 +35,8 @@ public class ContentServiceImpl implements ContentService {
         content.setUpdated(new Date());
         //插入到内容表
         contentMapper.insert(content);
+        //同步缓存
+        jedisClient.hdel(INDEX_CONTENT,content.getCategoryId().toString());
         return TaotaoResult.ok();
     }
 
@@ -46,10 +57,27 @@ public class ContentServiceImpl implements ContentService {
 
     @Override
     public List<TbContent> getContentByCid(long cid) {
+        //先查询缓存
+        try {
+            //查询到
+            String json = jedisClient.hget(INDEX_CONTENT, cid + "");
+            if (StringUtils.isNotBlank(json)){
+                List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+                return list;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         TbContentExample example = new TbContentExample();
         TbContentExample.Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(cid);
         List<TbContent> list = contentMapper.selectByExample(example);
+        //把结果添加到缓存
+        try{
+            jedisClient.hset(INDEX_CONTENT,cid+"", JsonUtils.objectToJson(list));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return list;
     }
 }
