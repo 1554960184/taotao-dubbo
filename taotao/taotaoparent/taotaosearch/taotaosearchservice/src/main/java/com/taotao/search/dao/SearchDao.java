fb5e75281cbe07c726c060767b99d7de4877e152
scxiong <1554960184@qq.com>
2020-09-20 00:46:00 +0800

    solr数据库的搭建以及solr dao层的实现

---
+++ taotao/taotaoparent/taotaosearch/taotaosearchservice/src/main/java/com/taotao/search/dao/SearchDao.java
@@ -0,0 +1,60 @@
+package com.taotao.search.dao;
+
+import com.taotao.pojo.SearchItem;
+import com.taotao.pojo.SearchResult;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 查询索引库dao
+ */
+@Repository
+public class SearchDao {
+    @Autowired
+    private SolrServer solrServer;
+    public SearchResult search(SolrQuery query)throws Exception{
+        //根据query对象进行查询
+        QueryResponse response = solrServer.query(query);
+        //取查询结果
+        SolrDocumentList solrDocuments = response.getResults();
+        //取查询结果总记录数
+        long numFound = solrDocuments.getNumFound();
+        SearchResult result = new SearchResult();
+        result.setRecordCount(numFound);
+        List<SearchItem>itemList = new ArrayList<>();
+        //把查询结果封装到SearchItem对象中
+        for (SolrDocument solrDocument:solrDocuments){
+            SearchItem item = new SearchItem();
+            item.setCategory_name((String) solrDocument.get("item_category_name"));
+            item.setId((String) solrDocument.get("id"));
+            item.setImage((String) solrDocument.get("item_image"));
+            item.setPrice((long) solrDocument.get("item_price"));
+            item.setSell_point((String) solrDocument.get("item_sell_point"));
+            //取高亮显示
+            Map<String, Map<String, List<String>>> highlighting = response.getHighlighting();
+            List<String> list = highlighting.get(solrDocument.get("id")).get("item_title");
+            String title = "";
+            if (list != null && list.size() > 0) {
+                title = list.get(0);
+            } else {
+                title = (String) solrDocument.get("item_title");
+            }
+            item.setTitle(title);
+            //添加到商品列表
+            itemList.add(item);
+        }
+        //把查询结果封装到SearchResult对象中
+        result.setItemList(itemList);
+        //返回
+        return result;
+    }
+}
